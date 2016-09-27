229de0a5f2518dba0a01ff8db0dd6f4726696eff
kylon <kylonux@gmail.com>
2016-09-27 09:33:50 +0200

    msm7x30-common: cmhw: Use FileUtils from CMSDK

--- cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
+++ cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
@@ -17,7 +17,7 @@
 package org.cyanogenmod.hardware;
 
 import java.io.File;
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 /*
  * Display RGB intensity calibration (kcal)
@@ -42,8 +42,7 @@ public class DisplayColorCalibration {
      */
 
     public static boolean isSupported() {
-        File f = new File(KCAL_PATH);
-        return f.exists();
+        return FileUtils.isFileReadable(KCAL_PATH) && FileUtils.isFileWritable(KCAL_PATH);
     }
 
     /*
--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -18,14 +18,14 @@ package org.cyanogenmod.hardware;
 
 import java.io.File;
 
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 public class VibratorHW {
 
     private static String AMP_PATH = "/sys/class/timed_output/vibrator/voltage_level";
 
     public static boolean isSupported() {
-        return new File(AMP_PATH).exists();
+        return FileUtils.isFileReadable(AMP_PATH) && FileUtils.isFileWritable(AMP_PATH);
     }
 
     public static int getMaxIntensity()  {
